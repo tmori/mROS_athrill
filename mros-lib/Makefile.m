b7c78d47ee0fee2910699000470ea1ca60fc282c
tmori@esm.co.jp <tmori@esm.co.jp>
2018-12-25 17:32:32 +0900

    add mros

---
+++ mros-lib/Makefile.m
@@ -0,0 +1,9 @@
+
+#CXX_OBJ += xmlparser.o
+#CXX_OBJ += xmlcall.o
+#CXX_OBJ += tcp_ros.o
+#CXX_OBJ += ros.o
+#CXX_OBJ += mros.o
+
+INCLUDE_PATHS += -I$(PRJROOT_MROS)
+
---
+++ sample_build/kernel_cfg.h
@@ -0,0 +1,6 @@
+#define PUB_TASK        1
+#define SUB_TASK        1
+#define XML_SLV_TASK    1
+#define XML_MAS_TASK    1
+#define USR_TASK1       1
+#define USR_TASK2       1
