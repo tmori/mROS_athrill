9e7dd99b34bf83a1e01a162df3090cead292dc00
tmori@esm.co.jp <tmori@esm.co.jp>
2018-12-28 09:34:50 +0900

    add bss_noclr

--- mros-lib/mros.cpp
+++ mros-lib/mros.cpp
@@ -19,7 +19,7 @@ const int m_port = 11311;	//ros master xmlrpc port
 
 /*********global variables***************/
 /***** shared memory *****/
-char mem[1024*1024*2];
+char mem[1024*1024*2]   __attribute__ ((section(".bss_noclr")));;
 extern std::vector<ID> IDv;
 int ros_sem =0;	//mROS resource semapho
 int count=1;	//for assign node ID
