51ffbd0ad8cf70fbb438cb407efd3ee6aef85381
tmori@esm.co.jp <tmori@esm.co.jp>
2019-07-10 19:44:39 +0900

    bug fixed for topic subscribe method

--- mros-lib/src/transfer/cimpl/mros_topic_data_subscriber_cimpl.c
+++ mros-lib/src/transfer/cimpl/mros_topic_data_subscriber_cimpl.c
@@ -4,6 +4,8 @@
 #include "mros_array_container.h"
 #include "mros_usr_config.h"
 
+MROS_ARRAY_CONTAINER_CONFIG_DECLARE_MANAGER(mros_topic_sub_mgr, MROS_TOPIC_MAX_NUM);
+
 static void mros_topic_subscribe(mRosTopicConnectorManagerType *mgrp, mRosNodeEnumType type, mRosContainerObjType topic_obj, mRosMemoryListEntryType *topic_data)
 {
 	mRosReturnType ret;
@@ -37,6 +39,7 @@ void mros_topic_data_subscriber_run(void)
 	mRosMemoryListEntryType *topic_data;
 	mRosTopicIdType topic_id;
 	mRosReturnType ret;
+	mros_uint32 i;
 
 	mgrp = mros_topic_connector_factory_get(MROS_TOPIC_CONNECTOR_SUB);
 	if (mgrp == MROS_NULL) {
@@ -45,17 +48,20 @@ void mros_topic_data_subscriber_run(void)
 
 	topic_obj = mros_topic_connector_get_topic_first(mgrp);
 	while (topic_obj != MROS_COBJ_NULL) {
-		ret = mros_topic_connector_get_topic(topic_obj, &topic_id);
+		mros_array_container_add(&mros_topic_sub_mgr, topic_obj);
+		topic_obj = mros_topic_connector_get_topic_next(mgrp, topic_obj);
+	}
+	for (i = 0; i < mros_topic_sub_mgr.count; i++) {
+		ret = mros_topic_connector_get_topic(mros_topic_sub_mgr.array[i], &topic_id);
 		if (ret != MROS_E_OK) {
 			continue;
 		}
 		ret = mros_topic_get_data(topic_id, &topic_data);
 		if (ret == MROS_E_OK) {
-			mros_topic_subscribe(mgrp, MROS_NODE_TYPE_INNER, topic_obj, topic_data);
-			mros_topic_subscribe(mgrp, MROS_NODE_TYPE_OUTER, topic_obj, topic_data);
+			mros_topic_subscribe(mgrp, MROS_NODE_TYPE_INNER, mros_topic_sub_mgr.array[i], topic_data);
+			mros_topic_subscribe(mgrp, MROS_NODE_TYPE_OUTER, mros_topic_sub_mgr.array[i], topic_data);
 			(void)mros_mem_free(topic_data->data.mgrp, topic_data);
 		}
-		topic_obj = mros_topic_connector_get_topic_next(mgrp, topic_obj);
 	}
 	return;
 }
